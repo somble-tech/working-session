b922e477fb605562ff7d3e7ed0f2d310117119b9
matt-somble <matt@kyltr.com>
2024-12-19 11:56:47 -0500

    add weekly schedule workouts and meditations data

---
+++ src/data/classes.js
@@ -0,0 +1,72 @@
+// used in Weekly Schedule "Workouts" tab
+export const weeklyClasses = {
+  monday: [{ id: "12-23-24-001", title: "42 Min Pilates + Sculpt", thumbnailUrl: "https://picsum.photos/168/130" }],
+  tuesday: [
+    {
+      id: "12-24-24-001",
+      title: "20 Min Full Body Standing Series (Pre & Postnatal Friendly)",
+      thumbnailUrl: "https://picsum.photos/168/130",
+    },
+  ],
+  wednesday: [
+    {
+      id: "12-25-24-001",
+      title: "21 Min Sculpting Inner/Out Thigh Series",
+      thumbnailUrl: "https://picsum.photos/168/130",
+    },
+  ],
+  thursday: [{ id: "12-26-24-001", title: "19 Min Go-To Abs", thumbnailUrl: "https://picsum.photos/168/130" }],
+  friday: [
+    { id: "12-27-24-001", title: "20 Min Anything But Basic Full Body", thumbnailUrl: "https://picsum.photos/168/130" },
+  ],
+  saturday: [
+    { id: "12-28-24-001", title: "21 Min Booty Build & Release", thumbnailUrl: "https://picsum.photos/168/130" },
+  ],
+  sunday: [{ id: "12-29-24-001", title: "11 Min Sculpted Arms", thumbnailUrl: "https://picsum.photos/168/130" }],
+};
+
+// used in Weekly Schedule "Meditations" tab
+export const weeklyMeditations = [
+  {
+    key: "new-001",
+    title: "13 Min Meditation: Anxiety Relief",
+    label: "New",
+    thumbnailUrl: "https://picsum.photos/168/130",
+  },
+  {
+    key: "walking-001",
+    title: "11 Min Walking Meditation: A Quick Shift",
+    label: "Walking",
+    thumbnailUrl: "https://picsum.photos/168/130",
+  },
+  {
+    key: "breathwork-001",
+    title: "14 Min Meditation: To Awaken Yourself Within",
+    label: "Breathwork",
+    thumbnailUrl: "https://picsum.photos/168/130",
+  },
+  {
+    key: "ground-001",
+    title: "12 Min Meditation: More Love",
+    label: "Ground",
+    thumbnailUrl: "https://picsum.photos/168/130",
+  },
+  {
+    key: "relax-001",
+    title: "7 Min Meditation: A Mental Moment for You",
+    label: "Relax",
+    thumbnailUrl: "https://picsum.photos/168/130",
+  },
+  {
+    key: "sleep-001",
+    title: "15 Min Meditation: Choosing Rest (Audio-Only)",
+    label: "Sleep",
+    thumbnailUrl: "https://picsum.photos/168/130",
+  },
+  {
+    key: "sound-bath-001",
+    title: "10 Min Meditation: A Moment to Come Back to Yourself",
+    label: "Sound Bath",
+    thumbnailUrl: "https://picsum.photos/168/130",
+  },
+];
